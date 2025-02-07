c0646d844f67dffa9003a18bfc96a6d875df48ff
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2025-02-07 17:10:04 +0200

    Generate calibration report.

--- python/cli/sai_cli.py
+++ python/cli/sai_cli.py
@@ -4,7 +4,7 @@ from .process.process import define_subparser as process_define_subparser
 from .record.record import define_subparser as record_define_subparser
 from .convert.convert import define_subparser as convert_define_subparser
 from .smooth import define_subparser as smooth_define_subparser
-from .calibrate import define_subparser as calibrate_define_subparser
+from .calibrate.calibrate import define_subparser as calibrate_define_subparser
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Spectacular AI command line tool')
