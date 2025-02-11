89b63167630977e3d23dc958c351277c9228df61
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2025-02-11 14:39:58 +0200

    Generate calibration report (#57)

--- python/cli/calibrate/calibrate.py
+++ python/cli/calibrate/calibrate.py
@@ -1,13 +1,19 @@
 """
 Calibrate an IMU-camera system using a calibration target
 """
+
 def call_calibrate(args):
     import spectacularAI
     from spectacularAI.calibration import convert_args, run
-    run(convert_args(args))
+    output = run(convert_args(args))
+    if output:
+        from .report import report
+        report(args, output)
 
 def define_subparser(subparsers):
     sub = subparsers.add_parser('calibrate', help=__doc__.strip())
     sub.set_defaults(func=call_calibrate)
-    from spectacularAI.calibration import define_args
-    return define_args(sub)
+    from spectacularAI.calibration import define_args as define_args_calibration
+    from .report import define_args as define_args_report
+    define_args_calibration(sub)
+    define_args_report(sub)
